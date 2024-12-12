f4d64c90e69451d730e48fec02520127f8a88fe7
zaka223glitch <kwakwa2321@gmail.com>
2024-12-12 13:44:42 +0100

    i added a nav bar and fixed some bugs

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-
 const audio = new Audio();
 audio.src = "./click-234708.mp3";
 
-audio.onended = function() {
-    window.location.href = './page2.html'; 
-};
-
 function playAudio() {
-    audio.play();  
+  audio.play();
+  audio.onended = function() {
+    window.location.href = './page2.html'; 
+  };
+}
+
+function toggleNav() {
+  const navMenu = document.querySelector('.nav-menu');
+  navMenu.classList.toggle('active');
 }
