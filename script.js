44302d5e5401276f889ef5ee4d723c44866722a3
zaka223glitch <kwakwa2321@gmail.com>
2024-12-16 14:51:14 +0100

    ik heb image gefict en knop met geluid toegevoegd

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-const audio = new Audio();
-audio.src = "./click-234708.mp3";
+const audio1 = new Audio();
+audio1.src = "./click-234708.mp3";
 
 function playAudio() {
-  audio.play();
-  audio.onended = function() {
-    window.location.href = './page2.html'; 
+  audio1.play();
+  audio1.onended = function () {
+    window.location.href = './page2.html';
   };
+}
+
+const audio2 = new Audio();
+audio2.src = "./mixkit-achievement-bell-600.wav";
+
+function playAudio1() {
+  audio2.play();
+  setTimeout(() => {
+    window.location.href = './page3.html';
+  }, 200);
 }
 
 function toggleNav() {
