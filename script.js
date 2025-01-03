3a0609639d791111e5ed5e05a8b5b8b9e728df78
zaka223glitch <kwakwa2321@gmail.com>
2025-01-03 21:05:34 +0100

    add sounds and more questions and made the pages more modern

--- script.js
+++ script.js
@@ -18,7 +18,43 @@ function playAudio1() {
   }, 200);
 }
 
+const audio3 = new Audio();
+audio3.src = "./mixkit-achievement-bell-600.wav";
+
+function playAudio2() {
+  audio3.play(); 
+  setTimeout(() => {
+    window.location.href = './page4.html';
+  }, 200);
+}
+
+function playAudio3() {
+  audio3.play(); 
+  setTimeout(() => {
+    window.location.href = './congrats.html';
+  }, 200);
+}
+
 function toggleNav() {
   const navMenu = document.querySelector('.nav-menu');
   navMenu.classList.toggle('active');
 }
+
+
+function Congrats() {
+  window.location.href = "congrats.html";
+}
+
+const gameOverAudio = new Audio();
+gameOverAudio.src = "./buzzer-or-wrong-answer-20582.mp3"; 
+
+function GameOverPage() {
+  gameOverAudio.play(); 
+  
+  gameOverAudio.onended = function () {
+    setTimeout(() => {
+      window.location.href = "gameover.html"; 
+    }, 200); 
+  };
+}
+
